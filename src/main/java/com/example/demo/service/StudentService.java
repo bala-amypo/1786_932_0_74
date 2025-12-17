81d1e99bb70303ccb18e56ff1bfec2383f27278d
bala-amypo <bala@amypo.in>
2025-12-17 09:19:47 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -7,6 +7,11 @@ import com.example.demo.entity.StudentEntity;
 public interface StudentService {
 
     public StudentEntity addStudents(StudentEntity student);
-    
+
     public List<StudentEntity> getStudents();
+
+    public StudentEntity getStudentById(Long id);
+
+    public void deleteStudentById(Long id);
+
 }
