3597186ace449d9acac1f956c86019c74c33acb0
bala-amypo <bala@amypo.in>
2025-12-19 04:54:45 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -12,6 +12,7 @@ public class StudentEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    
     private String email;
     private float cgpa;
 
